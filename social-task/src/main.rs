23149012aa4f27d1e25baba814dfcb821207c134
ynguyen <ndty14@gmail.com>
2024-08-27 04:00:16 +0700

    commit

--- social-task/src/main.rs
+++ social-task/src/main.rs
@@ -1,5 +1,5 @@
 use kafka::producer::{KafkaMessage, KafkaProducer};
-use kafka::consumer::{create_kafka_consumer, consume};
+use kafka::consumer::{create_kafka_consumer, consume_message};
 use kafka::config::KafkaConnectionConfig;
 
 
@@ -9,6 +9,6 @@ async fn main() {
     // let producer = KafkaProducer::new(&conn_config, String::from("test-redpanda"));
     // let _ = producer.produce(KafkaMessage {key: "username".to_string(), value: "tarik".to_string()}).await;
     let consumer = create_kafka_consumer(&conn_config, "test-redpanda", "group_test");
-    let _ = consume(consumer).await;
+    let _ = consume_message(consumer).await;
 }
 
