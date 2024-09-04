720b2375a5cf241bacda6b7098fb1a10cd32e138
ynguyen <ndty14@gmail.com>
2024-09-05 01:21:36 +0700

    Refactor

--- social-module/build.rs
+++ social-module/build.rs
@@ -1,8 +1,3 @@
-use std::error::Error;
-use std::{env, path::PathBuf};
+fn main() {
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    
-    Ok(())
 }
--- social-module/src/server.rs
+++ social-module/src/server.rs
@@ -2,10 +2,8 @@
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let store = social_store::store::Store::new("postgres://postgres:123@localhost:5432/postgres".to_string());
-
+    let store = social_store::store::Store::new("postgres://postgres:password@localhost:5432/postgres".to_string());
     let private_server = social_module::api::server::start_api_server(store);
-
     tokio::select! {
         _ = private_server => {}
     }
